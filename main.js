f6ec8d06b991baf96b5deaff24e667ff205e4e25
amalsaab <441102683@student.ksu.edu.sa>
2023-11-15 09:56:27 +0300

    init

---
+++ main.js
@@ -0,0 +1,7 @@
+// async function fetch_api(){
+//     const response = await fetch("https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=xta0jlRTraX5F2vwEggNneqyo0fzE05R")
+//     const data = await response.json();
+//     console.log(data);
+        
+// }
+// fetch_api()
